e92efe55fd4b739427c14b4589e3e48479b0bb55
Bohdan <bosrok.ke@gmail.com>
2025-02-23 12:10:14 +0000

    update navigation menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,18 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.querySelector('.js-menu-toggle');
   const menu = document.querySelector('.js-menu');
+  const overlay = document.createElement('div');
+  const menuLinks = document.querySelectorAll('.header-nav-list a');
 
-  menuToggle.addEventListener('click', function () {
-    const isOpen = menu.classList.contains('active');
+  overlay.classList.add('overlay');
+  document.body.appendChild(overlay);
 
-    if (isOpen) {
-      menu.style.maxHeight = '0';
-      setTimeout(() => menu.classList.remove('active'), 300);
-    } else {
-      menu.classList.add('active');
-      menu.style.maxHeight = menu.scrollHeight + 'px';
-    }
+  function toggleMenu() {
+    menu.classList.toggle('active');
+    menuToggle.classList.toggle('active'); // Переключаем состояние кнопки
+    overlay.classList.toggle('active');
+  }
 
-    menuToggle.classList.toggle('active');
+  menuToggle.addEventListener('click', toggleMenu);
+  overlay.addEventListener('click', toggleMenu);
+
+  // Закрытие меню при клике на любую ссылку
+  menuLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      setTimeout(toggleMenu, 300);
+    });
   });
 });
