2b690337bbd1b5c584dadf5291aa559d1e87e409
Hanna Lakus <annalakus@ukr.net>
2025-02-24 18:48:52 +0000

    changed go back button styles

--- src/js/back-to-home.js
+++ src/js/back-to-home.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   const btn = document.getElementById('btnGoHome');
-  console.dir(btn, 'btn');
 
-  window.addEventListener('scroll', function () {
-    if (window.scrollY > 50) {
-      btn.classList.add('scrolled');
-    } else {
-      btn.classList.remove('scrolled');
-    }
-  });
+  if (btn) {
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 50) {
+        btn.classList.add('scrolled');
+      } else {
+        btn.classList.remove('scrolled');
+      }
+    });
+  }
 });
